ed122a32fb787fe5378642c1555e57ee4fba3828
Yan Wu <ywuu0055@student.monash.edu>
2019-09-05 13:20:57 +1000

    Initial commit

---
+++ quit/Startup.cs
@@ -0,0 +1,14 @@
+﻿using Microsoft.Owin;
+using Owin;
+
+[assembly: OwinStartupAttribute(typeof(quit.Startup))]
+namespace quit
+{
+    public partial class Startup
+    {
+        public void Configuration(IAppBuilder app)
+        {
+            ConfigureAuth(app);
+        }
+    }
+}
