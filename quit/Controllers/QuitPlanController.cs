9f68db7bb7ea320f8ce66e28b0b22692ceb27d45
Jingchao Tan <jtan0053@student.monash.edu>
2019-10-06 19:10:04 +1100

    QuitPlan

---
+++ quit/Controllers/QuitPlanController.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+
+namespace quit.Controllers
+{
+    public class QuitPlanController : Controller
+    {
+        // GET: Benefit
+        public ActionResult QuitPlan()
+        {
+            return View();
+        }
+    }
+}
